257a3e24077cf2cb70a0d6f28cd073810eece851
fifitt <tiarabintang@gmail.com>
2022-11-14 08:24:22 +0700

    first commit

---
+++ app/src/main/java/com/example/ika18/layout_header_drawer.kt
@@ -0,0 +1,11 @@
+package com.example.ika18
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout_header_drawer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_header_drawer)
+    }
+}
